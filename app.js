85924299af75f22ac1f84a9b5209b1bcfe752c8d
KellyBaldonado <kellybaldonado14@gmail.com>
2017-02-22 19:51:18 -0600

    First website upload

---
+++ app.js
@@ -0,0 +1,21 @@
+
+(function(){
+	var app = angular.module("main", []);
+
+	app.controller("mainController", function () {
+
+		this.Message = "Kelly Baldonado's resume here"; 
+
+     });
+
+})();
+
+
+
+
+
+
+
+
+
+
